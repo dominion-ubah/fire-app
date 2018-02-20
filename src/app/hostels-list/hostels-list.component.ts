498e4b437afb62a165ab8720800b33ea89033dfc
dominion-ubah <domee.ubah@gmail.com>
2018-02-20 14:50:04 +0100

    stuff

--- src/app/hostels-list/hostels-list.component.ts
+++ src/app/hostels-list/hostels-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../auth.service';
 
@@ -12,29 +13,48 @@ export class HostelsListComponent implements OnInit {
  public hostelsObservable: Observable<any[]>;
  public hostels;
  public user$;
-  constructor(private db: AngularFireDatabase, private authService: AuthService) { }
+ public addHostelForm: FormGroup;
+  constructor(private db: AngularFireDatabase, public formBuilder: FormBuilder, private authService: AuthService) {
+      // form initialization
+this.addHostelForm = this.formBuilder.group({
+  name: ['', Validators.required],
+  serviceAgents: ['', Validators.required],
+});
+  }
 
   ngOnInit() {
     this.hostelsObservable = this.getHostelList('/hostels');
-    this.hostels = this.db.list('/hostels');
-    this.user$ = this.authService.user;
+     this.db.list('/hostels').valueChanges().subscribe(h => {
+      console.log('h:', h);
+      this.hostels = h;
+    });
+     this.authService.user.subscribe(u => {
+      console.log('u:', u) ;
+     this.user$ = u.email;
+    });
   }
 getHostelList(route) {
   return this.db.list(route).valueChanges();
 }
-addHostel(field) {
-  this.hostels.push(
-    {
-      text: field.value,
-      user: this.user$.map(user => {
-        return user.email;
-      }),
-      createdAt: (new Date).toString(),
-      likes: 0
+// addHostel(field) {
+//   this.hostels.push(
+//     {
+//       text: field.value,
+//       user: this.user$.map(user => {
+//         return user.email;
+//       }),
+//       createdAt: (new Date).toString(),
+//       likes: 0
 
-    }
-  );
-  field.value = ' ';
+//     }
+//   );
+//   field.value = ' ';
+// }
+addHostel() {
+  const inputV = this.addHostelForm.value;
+  console.log(inputV);
+  this.hostels.push(inputV);
+  console.log(this.hostels);
 }
 like($key: string, count: number) {
  this.hostels.update($key, {likes: count + 1} );
